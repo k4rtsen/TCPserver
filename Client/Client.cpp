6b0b8d154e69c2673359a43c4ba9142d5eec55a5
Kartsen <karen.ayv12599@gmail.com>
2022-02-19 16:47:58 +0300

    first working simple system server-client

---
+++ Client/Client.cpp
@@ -0,0 +1,37 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	//WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+		cout << "Error: failed connect to server.\n";
+		return 1;
+	}	
+	cout << "Connected!\n";
+	char msg[256];
+	recv(Connection, msg, sizeof(msg), NULL);
+	cout << "Server: " << msg << endl;
+
+	system("pause");
+	return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,42 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	//WSAStartup
+	WSAData wsaData;
+	WORD DLLVersion = MAKEWORD(2, 1);
+	if (WSAStartup(DLLVersion, &wsaData) != 0) {
+		cout << "Error" << endl;
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET; // семейство протоколов
+
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // 
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr)); // 
+	listen(sListen, SOMAXCONN); // прослушивание
+
+	SOCKET newConnection;
+	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+
+	if (newConnection == 0) {
+		cout << "Error #2\n";
+	}
+	else {
+		cout << "Client Connected!\n";
+		char msg[256] = "Hello. It`s my first network program!";
+		send(newConnection, msg, sizeof(msg), NULL);
+	}
+
+	system("pause");
+	return 0;
+}
