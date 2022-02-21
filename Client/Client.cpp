9231d9f44b5b7bf2a220e27c425c89f1ec02217b
Kartsen <karen.ayv12599@gmail.com>
2022-02-21 03:31:59 +0300

    some changing

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -1,13 +1,12 @@
 ﻿#pragma comment(lib, "ws2_32.lib")
 #include <winsock2.h>
 #include <iostream>
-#include <string>
 
 #pragma warning(disable: 4996)
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main() {
 	//WSAStartup
 	WSAData wsaData;
 	WORD DLLVersion = MAKEWORD(2, 1);
@@ -16,22 +15,48 @@ int main(int argc, char* argv[]) {
 		exit(1);
 	}
 
+	const char* ip = "127.0.0.1";
+	unsigned short portUDP = 6000, portTCP = 5555;
+	char fileName[256] = "test.txt";
+	unsigned short timeOut = 500;
+	cout << "The following settings are used to connect to the server:\n\tIP: " << ip << "\n\tportTCP: " << portTCP << "\n\tportUDP: " << portUDP << endl;
+	cout << "Other settings:\n\tFile name: " << fileName << "\n\tConfirmation waiting time: " << timeOut << "ms" << endl;
+
+	// адрес сервера
 	SOCKADDR_IN addr;
 	int sizeofaddr = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addr.sin_port = htons(1111);
+	addr.sin_addr.s_addr = inet_addr(ip);
+	addr.sin_port = htons(portTCP);
 	addr.sin_family = AF_INET;
+	//---
 
-	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
-	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+	// создание TCP сокета
+	SOCKET TCPconn = socket(AF_INET, SOCK_STREAM, NULL);
+	// соединение с сервером
+	if (connect(TCPconn, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
 		cout << "Error: failed connect to server.\n";
 		return 1;
-	}	
-	cout << "Connected!\n";
-	char msg[256];
-	recv(Connection, msg, sizeof(msg), NULL);
-	cout << "Server: " << msg << endl;
+	}
+	else {
+		// обмен данными
+		cout << "Connected!\n";
+		char msg[256];
+		recv(TCPconn, msg, sizeof(msg), NULL);
+		cout << "Server: " << msg << endl;
 
+		// загрузка файла в память
+
+
+		// отправка имени и размера файла, а также порта для UDP сокета
+		send(TCPconn, fileName, sizeof(msg), NULL);
+		// send file size		
+		send(TCPconn, itoa(portUDP, new char, 10), sizeof(msg), NULL);
+
+		// создание UDP сокета
+
+
+
+	}
+	closesocket(TCPconn);
 	system("pause");
-	return 0;
 }
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -6,37 +6,61 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main() {
 	//WSAStartup
+	// загрузка необходимой библиотеки
 	WSAData wsaData;
 	WORD DLLVersion = MAKEWORD(2, 1);
 	if (WSAStartup(DLLVersion, &wsaData) != 0) {
 		cout << "Error" << endl;
 		exit(1);
 	}
+	//---
 
+	const char* ip = "127.0.0.1";
+	unsigned short portTCP = 5555;
+	char download_dir[256] = "temp";
+
+	cout << "The server is ready to go, your default settings:\n\tIP: " << ip << "\n\tport: " << portTCP << "\n\tDownload directory: " << download_dir << endl;
+
+	// адресс
 	SOCKADDR_IN addr;
 	int sizeofaddr = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	addr.sin_port = htons(1111);
-	addr.sin_family = AF_INET; // семейство протоколов
+	addr.sin_addr.s_addr = INADDR_ANY; //inet_addr(ip);
+	addr.sin_port = htons(portTCP);// если задано 0, то ОС сама выделит свободный номер порта для сокета
+	addr.sin_family = AF_INET; // семейство протоколов, всегда равно AF_INET или AF_INET6 для IPv6
+	//---
 
-	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // 
-	bind(sListen, (SOCKADDR*)&addr, sizeof(addr)); // 
-	listen(sListen, SOMAXCONN); // прослушивание
+	// создание TCP сокета
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // создает безымянный сокет, определяющий домен (1й арг), тип (2й арг) и протокол (3й арг) подключения
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr)); // связываем сокет с адресом хоста и портом 
+	listen(sListen, SOMAXCONN); // прослушивание (SOMAXCONN содержит длину очереди входящих запросов на установление соединения (кол подключаемых устройств))
 
-	SOCKET newConnection;
-	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+	cout << "Waiting for a connection..." << endl;
+	SOCKET TCPconn;
+	TCPconn = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr); // извлечение запросов на подключение из прослушивания
+	//---
 
-	if (newConnection == 0) {
+	if (TCPconn == 0) {
 		cout << "Error #2\n";
 	}
 	else {
+		char fileName[256];
+		char portUDP[256];
+
+		// передача данных...
 		cout << "Client Connected!\n";
-		char msg[256] = "Hello. It`s my first network program!";
-		send(newConnection, msg, sizeof(msg), NULL);
+		char msg[256] = "The server is ready to accept the file!";
+		send(TCPconn, msg, sizeof(msg), NULL);
+
+		// принятие имени и размера файла, а также порта для UDP сокета
+		recv(TCPconn, fileName, sizeof(msg), NULL);
+		// accept file size
+		recv(TCPconn, portUDP, sizeof(msg), NULL);
+		cout << "Client: \n\tUDPport: " << portUDP << "\n\tFile name: " << fileName << endl;
 	}
 
+	closesocket(TCPconn);
+	closesocket(sListen);
 	system("pause");
-	return 0;
 }
